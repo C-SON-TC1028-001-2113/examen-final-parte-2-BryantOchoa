4212d9f40c03cf4e4d28bd57d77cf3c21cb623d1
BryantOchoa <A01252419@itesm.mx>
2021-10-15 17:52:32 +0000

    codigo 2

--- assignments/15SumaColumnas/README.md
+++ assignments/15SumaColumnas/README.md
@@ -1,3 +1,4 @@
+
 ![Tec de Monterrey](../../images/logotecmty.png)
 # Suma columnas
 Matrices suma columnas
--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,8 +1,27 @@
-
+def crearm():
+    lista = []
+    row = int(input())
+    column = int(input())
+    if row > 0 and column > 0: 
+        for i in range(row):
+            lista.append([])
+            for j in range(column):
+                n = int(input())
+                lista[i].append(n)
+    else: 
+        print('Error')
+    return lista
 
 def main():
-
-
+    m = crearm()
+    collist = []
+    if len(m) > 0:
+        for i in range(len(m[0])):
+            count = 0
+            for j in range(len(m)):
+                count += m[j][i] 
+            collist.append(count)
+        print(collist)
 
 if __name__=='__main__':
     main()
